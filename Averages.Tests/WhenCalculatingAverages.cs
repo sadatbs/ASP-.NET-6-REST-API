0a5b276c2877f9373eb109370e4b84189940d1d7
Sadat Bin Sharfuddin <sadat.sharfuddin@streamstech.com>
2024-01-08 21:12:56 +0600

    update tests

---
+++ Averages.Tests/WhenCalculatingAverages.cs
@@ -0,0 +1,22 @@
+namespace Averages.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void SingleInputShouldProduceSameValueAsResult()
+    {
+        string[] inputs = {"1"};
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(1.0, result, 1E-14);
+    }
+
+    [TestMethod]
+    public void MultipleInputsShouldProduceAverageAsResult()
+    {
+        string[] inputs = { "1", "2", "3" };
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(2.0, result, 1E-14);
+    }
+    
+}
